e6104323443f7e95b370d2cda8badc7ab278d719
whenqy <13121600523@163.com>
2019-11-25 10:51:27 +0800

    123123

---
+++ nacosuser/src/test/groovy/com/wen/nacosuser/NacosuserApplicationTests.groovy
@@ -0,0 +1,12 @@
+package com.wen.nacosuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NacosuserApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
